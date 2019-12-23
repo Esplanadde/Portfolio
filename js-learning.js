2fe51b88669db219dfadca52e9c625f8c7f2c62e
Esplanadde <esplanadde@gmail.com>
2019-12-23 21:55:28 +0300

    Added first jquery script on About Myself page, some amends to CSS

--- js-learning.js
+++ js-learning.js
@@ -1,1 +1,6 @@
-console.log(1000);
+$(document).ready(function() {
+    var element = $('#dred');
+    $(window).scroll(function(){
+      element['fade'+ ($(this).scrollTop() > 200 ? 'In': 'In')](1000);
+  });
+});
