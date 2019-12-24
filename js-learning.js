9db9f15086e1e9837825fb716865eed9c1973847
Esplanadde <esplanadde@gmail.com>
2019-12-24 15:56:56 +0300

    Testing animation...FAILED

--- js-learning.js
+++ js-learning.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var element = $('#dred');
-    $(window).scroll(function(){
-      element['fade'+ ($(this).scrollTop() > 500 ? 'In': 'In')](1000);
-  });
+    $('#bblue').$("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 100
+       });
 });
