ddd57a37fb69d2e0c9111f1c1c46ddf94ae0f024
Esplanadde <esplanadde@gmail.com>
2019-12-28 18:00:03 +0300

    Add contacts

--- js-learning.js
+++ js-learning.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     var element = $('#bblue');
     $(window).scroll(function(){
-        element['fade'+ ($(this).scrollTop() > 500 ? 'In': 'Out')](1000);
+        element['fade'+ ($(this).scrollTop() > 500 ? 'In': 'In')](1000);
     });
 
     var element2 = $('#bred');
     $(window).scroll(function() {
-        element2['fade'+ ($(this).scrollTop()>700 ? 'In' : 'Out')](1000);
+        element2['fade'+ ($(this).scrollTop()>700 ? 'In' : 'In')](1000);
     });
 });
